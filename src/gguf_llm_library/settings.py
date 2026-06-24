7ae11e4c812271714efa46c2db19e61e4053414c
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-06-24 09:57:19 +0200

    Use Pi size

--- src/gguf_llm_library/settings.py
+++ src/gguf_llm_library/settings.py
@@ -4,12 +4,16 @@ MODELS = [
         "filename": "qwen2.5-1.5b-instruct-q6_k.gguf", #1.46GB
     },
     {
-        "repo_id": "squ11z1/Mythos-nano",
+        "repo_id": "squ11z1/Mythos-nano", #3B
         "filename": "mythos-nano-Q4_K_M.gguf", #1.93GB
     },
     {
         "repo_id": "bartowski/Mistral-7B-Instruct-v0.3-GGUF",
-        "filename": "Mistral-7B-Instruct-v0.3-IQ4_NL.gguf", #4.13GB
+        "filename": "Mistral-7B-Instruct-v0.3-IQ3_M.gguf", #3.29GB, possible Pi size
+    },
+    {
+        "repo_id": "Jackrong/Qwopus3.5-9B-Coder-GGUF", #claims class leader
+        "filename": "Qwopus3.5-9B-coder-Exp-IQ4_XS.gguf", #5.23GB
     },
 ]
 N_CTX = 4 * 1024
